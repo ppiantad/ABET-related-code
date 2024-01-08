3c72fbca2d212503c2307fa5bc5ee72aebde2832
ppiantad <ptpiantadosi@smcm.edu>
2024-01-07 23:31:04 -0500

    updates to raster generator

--- RDT_raster_figure_generation.m
+++ RDT_raster_figure_generation.m
@@ -1,10 +1,10 @@
 %Plot multiple graphs using rasterFromBehav
-close all; clear all; clc
+% close all; clear all; clc
 
 %for right now: if Incorrect A is Sprior, use "rasterFromBehavREV2"
 %if Incorrect A is Snever, use "rasterFromBehav"
 
 figure
 % subplot(3,2,1)
-title('RDT Photometry')
-[LargeRew,SmallRew,Shock,Omission,yyLarge, concat_all] = raster_RDT('RRD398 10062023 ABET.csv'); %early Disc
+title('RDT Behavior')
+[LargeRew,SmallRew,Shock,Omission,yyLarge, concat_all] = raster_RDT('BLA-INSC-27 12302022 ABET.csv'); %early Disc
